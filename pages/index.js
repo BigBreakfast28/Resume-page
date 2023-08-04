35d6377ae7264398a55726836e7d1aa1f93a275a
Cameron Dean <Deancameron99@gmail.com>
2023-08-03 23:39:19 -0400

    Adding a few images along with some text

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Nav from "@/componets/Nav";
 export default function Home() {
   return (
     <div className='bg-blue-800 w-screen h-screen flex items-center text-white'>
-      <div>Welcome to my Resume Page!</div>
       <Nav />
     </div>
   )
