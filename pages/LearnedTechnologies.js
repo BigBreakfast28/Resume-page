faf85397cf2e32530e32913c928ce4fbadbfe005
Cameron <deancameron99@gmail.com>
2023-08-30 23:36:36 -0400

    Setting up work exp page

--- pages/LearnedTechnologies.js
+++ pages/LearnedTechnologies.js
@@ -3,6 +3,11 @@ import Home from "@/pages";
 export default function LearnedTechnologies() {
     <Home>Technologies Page </Home>
     return(
-        <h1>LearnedTechnologies</h1>
+        <>
+            <div className="bg-blue-900 h-screen">
+                <h1 className="text-center">LearnedTechnologies</h1>
+            </div>
+        </>    
     )
+
 }
